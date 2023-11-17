696f960d1a6c4033d52f09a9c65145f797091870
Atharv Pathak <atharvpathak248@gmail.com>
2023-11-17 12:08:44 +0530

    C Codes

---
+++ C/Calculator.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    char operator;
+    double num1, num2, result;
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf("%c", &operator);
+
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &num1, &num2);
+
+    if (operator == '+') {
+        result = num1 + num2;
+    } else if (operator == '-') {
+        result = num1 - num2;
+    } else if (operator == '*') {
+        result = num1 * num2;
+    } else if (operator == '/') {
+        if (num2 != 0) {
+            result = num1 / num2;
+        } else {
+            printf("Error: Division by zero\n");
+            return 1; // Exit with an error code
+        }
+    } else {
+        printf("Error: Invalid operator\n");
+        return 1; // Exit with an error code
+    }
+
+    printf("Result: %lf\n", result);
+
+    return 0;
+}
---
+++ C/main.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("hello you are now looking in my repository");
+    return 0;
+}
