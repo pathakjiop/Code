13c0cd32e13b5bf98d722b03121a8ea81e98a354
Atharv Pathak <atharvpathak248@gmail.com>
2023-11-20 22:09:55 +0530

    new code

---
+++ CPP/StarPattern.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+void print_star_pattern(int rows) {
+    for (int i = 1; i <= rows; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main() {
+    int rows;
+    std::cout << "Enter the number of rows: ";
+    std::cin >> rows;
+    print_star_pattern(rows);
+    return 0;
+}
