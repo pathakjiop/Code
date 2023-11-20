e552b3f13f49611a4a5b857b8aee931a7ef8efe9
Atharv Pathak <atharvpathak248@gmail.com>
2023-11-20 22:08:51 +0530

    New Code

---
+++ C/StarPattern.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void print_star_pattern(int rows) {
+    for (int i = 1; i <= rows; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int rows;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    print_star_pattern(rows);
+    return 0;
+}
