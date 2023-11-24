6ef52699a7e374d47f61040519bbbb1acfe24ccd
Atharv Pathak <atharvpathak248@gmail.com>
2023-11-24 18:17:55 +0530

    Miner changes

--- Python/StarPattern.py
+++ Python/StarPattern.py
@@ -2,5 +2,5 @@ def print_star_pattern(rows):
     for i in range(1, rows + 1):
         print("*" * i)
 
-rows = int(input("Enter the number of rows: "))
+rows = int(input("Enter the number of rows:-"))
 print_star_pattern(rows)
