db586ff182928e2248493903d7d62d29bea93104
Atharv Pathak <atharvpathak248@gmail.com>
2023-11-26 16:34:22 +0530

    user input

--- JavaScript/basicoperations.js
+++ JavaScript/basicoperations.js
@@ -1,14 +1,15 @@
-// Arithmetic operations
-var num1 = 10;
-var num2 = 5;
+// Prompt the user for input
+var num1 = parseFloat(prompt("Enter the first number:"));
+var num2 = parseFloat(prompt("Enter the second number:"));
 
-var sum = num1 + num2; // Addition
-var difference = num1 - num2; // Subtraction
-var product = num1 * num2; // Multiplication
-var quotient = num1 / num2; // Division
+// Check if the input is valid numbers
+if (!isNaN(num1) && !isNaN(num2)) {
+  // Perform the addition
+  var sum = num1 + num2;
 
-// Printing the results to the console
-console.log("Sum:", sum);
-console.log("Difference:", difference);
-console.log("Product:", product);
-console.log("Quotient:", quotient);
+  // Display the result
+  console.log("The sum of " + num1 + " and " + num2 + " is: " + sum);
+} else {
+  // Handle invalid input
+  console.log("Invalid input. Please enter valid numbers.");
+}
