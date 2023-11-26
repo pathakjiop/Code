c69613d952601a9d6b4d2cd9af29055a2b04fcc2
Atharv Pathak <atharvpathak248@gmail.com>
2023-11-26 16:31:15 +0530

    main

--- C/main.c
+++ C/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
-
-int main() {
-    printf("hello world");
+ 
+// Driver code
+int main()
+{
+    int x, y,result;
+ 
+    x = 136;
+    y = 10;
+ 
+    // modulo for floating point values
+    result = x % y;
+    printf("%d", result);
+ 
     return 0;
 }
