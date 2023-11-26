663a04d5714f709ddb747479a66562709a0b280e
Atharv Pathak <atharvpathak248@gmail.com>
2023-11-26 16:34:02 +0530

    idk

---
+++ JavaScript/basicoperations.js
@@ -0,0 +1,14 @@
+// Arithmetic operations
+var num1 = 10;
+var num2 = 5;
+
+var sum = num1 + num2; // Addition
+var difference = num1 - num2; // Subtraction
+var product = num1 * num2; // Multiplication
+var quotient = num1 / num2; // Division
+
+// Printing the results to the console
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
