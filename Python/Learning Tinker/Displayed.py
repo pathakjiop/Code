828ac5818578a0aee4d7be8df2bc25ff1d0a620c
Atharv Pathak <atharvpathak248@gmail.com>
2023-12-02 15:07:30 +0530

    Learn

---
+++ Python/Learning Tinker/Displayed.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+def on_button_click():
+    print("Button clicked!")
+
+# Create the main window
+root = tk.Tk()
+root.title("Simple Tkinter Example")
+
+# Create a button and pack it into the window
+button = tk.Button(root, text="Click me!", command=on_button_click)
+button.pack(pady=20, padx=40)
+
+# Run the Tkinter event loop
+root.mainloop()
