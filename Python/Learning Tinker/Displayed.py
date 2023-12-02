6b2546cbdfaf331b85d8ef6db3193cdf6551b006
Atharv Pathak <atharvpathak248@gmail.com>
2023-12-02 15:08:37 +0530

    miner change

--- Python/Learning Tinker/Displayed.py
+++ Python/Learning Tinker/Displayed.py
@@ -8,7 +8,7 @@ root = tk.Tk()
 root.title("Simple Tkinter Example")
 
 # Create a button and pack it into the window
-button = tk.Button(root, text="Click me!and you will get a message in conslole", command=on_button_click)
+button = tk.Button(root, text="Click me and you will get a message in conslole", command=on_button_click)
 button.pack(pady=20, padx=40)
 
 # Run the Tkinter event loop
