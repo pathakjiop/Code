0d9fdfc32ddbdc27845b1c6f2f99d28aad94fe01
Atharv Pathak <atharvpathak248@gmail.com>
2023-12-03 11:38:41 +0530

    miner change

--- C/StarPattern.c
+++ C/StarPattern.c
@@ -16,3 +16,38 @@ int main() {
     print_star_pattern(rows);
     return 0;
 }
+
+/*#include <stdio.h>
+
+void print_star_pattern(int rows) {
+    for (int i = 1; i <= rows; ++i) {
+            printf("*");
+        printf("\n");
+    }
+}
+
+int main() {
+    int rows;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    print_star_pattern(rows);
+    return 0;
+}*/
+/*#include <stdio.h>
+
+void print_star_pattern(int rows) {
+    for (int i = 1; i <= rows; ++i) {
+        for (int j = 1; j <= rows; ++j) {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int rows;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    print_star_pattern(rows);
+    return 0;
+}*/
