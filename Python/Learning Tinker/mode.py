db447063cf5e090a5aa93413c17320806aeab7e7
Atharv Pathak <atharvpathak248@gmail.com>
2023-12-05 20:59:57 +0530

    made a quit botton

--- Python/Learning Tinker/mode.py
+++ Python/Learning Tinker/mode.py
@@ -1,8 +1,13 @@
 from customtkinter import *
-import tkinter as tk
+from tkinter import *
 
 def on_button_click():
     print("Button clicked!")
+
+def Close(): 
+    CTk.destroy(app)
+
+
 
 app=CTk()
 app.geometry("500x400")
@@ -15,7 +20,7 @@ but = CTkButton(master=app, text="Click Me", text_color="#000000"
 
 quitbut = CTkButton(master=app, text="quit", text_color="#000000"
                 ,corner_radius=32, fg_color="#9EFF00", 
-                hover_color="#FF9B00",command=on_button_click.quit)
+                hover_color="#FF9B00",command=Close)
 
 
 but.place(relx=0.5, rely=0.5, anchor="center")
