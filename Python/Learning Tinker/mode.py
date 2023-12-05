93824576945d3a8cb05024e3f62fa0ddf12f7725
Atharv Pathak <atharvpathak248@gmail.com>
2023-12-05 20:47:42 +0530

    Miner Change

--- Python/Learning Tinker/mode.py
+++ Python/Learning Tinker/mode.py
@@ -13,7 +13,12 @@ but = CTkButton(master=app, text="Click Me", text_color="#000000"
                 ,corner_radius=32, fg_color="#9EFF00", 
                 hover_color="#FF9B00",command=on_button_click)
 
+quitbut = CTkButton(master=app, text="quit", text_color="#000000"
+                ,corner_radius=32, fg_color="#9EFF00", 
+                hover_color="#FF9B00",command=on_button_click.quit)
+
 
 but.place(relx=0.5, rely=0.5, anchor="center")
+quitbut.pack(side=BOTTOM, padx=15, pady=20)
 
 app.mainloop()
