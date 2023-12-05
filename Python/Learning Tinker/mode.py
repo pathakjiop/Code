de303659b9eeb47f069770ce9e3c7f0f6ca11507
Atharv Pathak <atharvpathak248@gmail.com>
2023-12-05 19:00:49 +0530

    Basic

--- Python/Learning Tinker/mode.py
+++ Python/Learning Tinker/mode.py
@@ -1,11 +1,16 @@
 from customtkinter import *
+import tkinter as tk
+
+def on_button_click():
+    print("Button clicked!")
 
 app=CTk()
 app.geometry("500x400")
 
 set_appearance_mode("dark")
 
-but = CTkButton(master=app,text="Click Me", corner_radius=32, fg_color="#C850C0",hover_color="#4158D0")
+but = CTkButton(master=app, text="Click Me", text_color="#000000",corner_radius=32, fg_color="#9EFF00", hover_color="#FF9B00",command=on_button_click)
+
 
 but.place(relx=0.5, rely=0.5, anchor="center")
 
