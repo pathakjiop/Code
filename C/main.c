ed9079db22b931663efbe66f3e849e4d5e6cd180
pathakjiop <atharvpathak248@gmail.com>
2023-12-07 10:02:05 +0530

    Miner Changes

--- C/main.c
+++ C/main.c
@@ -1,16 +1,7 @@
 #include <stdio.h>
- 
-// Driver code
+
 int main()
 {
-    int x, y,result;
- 
-    x = 136;
-    y = 10;
- 
-    // modulo for floating point values
-    result = x % y;
-    printf("%d", result);
- 
+    printf("Hello World");
     return 0;
 }
